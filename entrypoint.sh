1ac247a8e5318c61e807ff4a862658b12245b959
lookatout <118869895+lookatout@users.noreply.github.com>
2023-04-20 09:23:14 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ NEZHA_PORT=
 NEZHA_KEY=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"f2b1b993ad19cbb0c5c84a2cd633e202","TunnelSecret":"CaM4gNf8U0CixvT/AnlsF7BvK2LCrGcVg75332RjvJs=","TunnelID":"f0bffbef-271d-4d37-95eb-1988095aa071"}'
+ARGO_DOMAIN=git1.xfpq.ga
 
 generate_config() {
   cat > config.json << EOF
